c9ded5010942a54035ccb93c8945733ae9fd09d1
Dan Nguyen <thanhdanhvc1403@gmail.com>
2022-10-03 00:23:39 +0700

    BT1.r

---
+++ test.r
@@ -0,0 +1,4 @@
+data_1 <- data.frame(x1 = c(7, 2, 8, 3, 3, 7),                            # Create data frame
+                     x2 = c("x", "y", "x", "x", "x", "y"),
+                     x3 = 11:16)
+data_1  
